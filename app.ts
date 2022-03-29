d5d741a991afbea6e73ac86b0d94439361b01883
thespielplatz <informatics@gmx.net>
2022-03-29 23:05:17 +0200

    Rainbow Background

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ import RenderFactory from './src/renderer/RenderFactory'
 import Display from './src/Display'
 import { WRITE_LINE_MODE } from './src/defines'
 import ConsoleRenderer from './src/renderer/ConsoleRenderer'
+import Rainbow from './src/animations/Rainbow'
 
 const RENDER_MACHINE: string = process.env.RENDERER || 'stubrenderer'
 const PIXEL_WIDTH: number = Number(process.env.PIXEL_WIDTH || 0)
@@ -44,19 +45,6 @@ const display = new Display(render, PIXEL_WIDTH, PIXEL_HEIGHT)
 display.writeColor = 0xFFFFFF
 display.writeLineMode = WRITE_LINE_MODE.INSTANT
 
-/*
-;(async function() {
-  display.writeLine('0123456789')
-  await pause(10000)
-  render.deinit()
-})()
-*/
-
-setInterval(function () {
-  rainbowBackground()
-  display.writeBuffer()
-}, 1000 / 30)
-
 // Test Foreground
 const updateText = () => {
   axios.get('https://blockstream.info/api/blocks/tip/height').then((response) => {
@@ -68,27 +56,16 @@ const updateText = () => {
 updateText()
 
 // Test Background
-// ---- animation-loop
-let offset = 0
-let pixelData = new Array(NUM_PIXEL)
-//setInterval(function () {
+const animate = new Rainbow(NUM_PIXEL)
+
 const rainbowBackground = () => {
-  for (let i = 0; i < NUM_PIXEL; i++) {
-    pixelData[i] = colorwheel((offset + i) % 256);
-  }
-
-  offset = (offset + 1) % 256;
-
-  display.writeBackgroundPixelData(pixelData)
+  display.writeBackgroundPixelData(animate.getNext())
 }
 
-function colorwheel(pos: number) {
-  pos = 255 - pos;
-  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3) }
-  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3) }
-  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0) }
+const update = () => {
+  rainbowBackground()
+  display.writeBuffer()
+  setTimeout(update, 1000 / 30)
 }
 
-function rgb2Int(r: number, g: number, b: number) {
-  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff)
-}
+update()
