1863b3f68b97d61ecc8499e9af5071b51d2da180
thespielplatz <informatics@gmx.net>
2022-03-29 23:06:20 +0200

    Rainbow Background

--- src/animations/Rainbow.ts
+++ src/animations/Rainbow.ts
@@ -8,28 +8,28 @@ class Rainbow {
 
   getNextSingle(): number[] {
     this.offset = (this.offset + 1) % 256
-    return this.pixelData.fill(this.colorwheel(this.offset))
+    return this.pixelData.fill(Rainbow.colorwheel(this.offset))
   }
 
   getNext(): number[] {
     for (let i = 0; i < this.pixelData.length; i++) {
-      this.pixelData[i] = this.colorwheel((this.offset + i) % 256);
+      this.pixelData[i] = Rainbow.colorwheel((this.offset + i) % 256);
     }
 
     this.offset = (this.offset + 1) % 256
     return this.pixelData
   }
-  private colorwheel(startPos: number) {
+  private static colorwheel(startPos: number) {
     let pos = 255 - startPos
     if (pos < 85) {
-      return this.rgb2Int(255 - pos * 3, 0, pos * 3)
+      return Rainbow.rgb2Int(255 - pos * 3, 0, pos * 3)
     }
     if (pos < 170) {
       pos -= 85
-      return this.rgb2Int(0, pos * 3, 255 - pos * 3)
+      return Rainbow.rgb2Int(0, pos * 3, 255 - pos * 3)
     }
     pos -= 170
-    return this.rgb2Int(pos * 3, 255 - pos * 3, 0)
+    return Rainbow.rgb2Int(pos * 3, 255 - pos * 3, 0)
   }
 
   private static rgb2Int(r: number, g: number, b: number) {
