091d35ed724447e6ca2fd33be65b605223ddb044
Thomas Schagerl <thomas.schagerl@gmx.net>
2022-10-03 18:51:21 +0200

    [FEATURE] add username to blocktris-client

--- blocktris-client/src/pages/PageIndex.vue
+++ blocktris-client/src/pages/PageIndex.vue
@@ -6,6 +6,8 @@
     >
       Blocktris
     </HeadlineDefault>
+    <p v-if="username != null">Playing as {{ username }}</p>
+    <p v-else-if="playingAsGuest">Playing as guest</p>
     <div v-if="connecting">
       <p>by <a href="https://satoshiengineering.com" target="_blank">Satoshi Engineering</a></p>
     </div>
@@ -33,8 +35,35 @@
       >Login via LNURL-auth</button>
     </div>
     <div
+      v-else-if="username == null && !playingAsGuest"
+      class="flex-1 grid place-items-center"
+    >
+      <p v-if="checkingUsername || settingUsername">Loading ...</p>
+      <div v-else>
+        <input
+          v-model="newUsername"
+          type="text"
+          class="w-full border my-1 px-3 py-2 focus:outline-none"
+          placeholder="Your name"
+          :disabled="settingUsername"
+        >
+        <button
+          class="bg-grey p-3"
+          :disabled="settingUsername"
+          @click="setUsername"
+        >Set my name</button>
+        <br>
+        OR
+        <br>
+        <button
+          class="bg-grey p-3"
+          @click="playAsGuest"
+        >Play as guest</button>
+      </div>
+    </div>
+    <div
       class="game-grid grid min-h-screen w-full"
-      v-else
+      v-else-if="playing"
     >
       <HeadlineDefault
         class="[grid-area:header]"
@@ -58,6 +87,15 @@
         @mouseup="downReleased()"
       >DOWN</button>
     </div>
+    <div
+      v-else
+      class="flex-1 grid place-items-center"
+    >
+      <button
+        class="bg-grey p-3"
+        @click="play()"
+      >Play</button>
+    </div>
   </div>
 </template>
 
@@ -75,12 +113,22 @@ const authenticating = ref(false)
 const lnurlHash = ref<string>()
 const lnurlEncoded = ref<string>()
 const authKey = ref<string>()
+const checkingUsername = ref(false)
+const username = ref<string>()
+const newUsername = ref<string>()
+const settingUsername = ref(false)
+const playingAsGuest = ref(false)
 
 const resetAuth = () => {
   authenticating.value = false
   lnurlHash.value = undefined
   lnurlEncoded.value = undefined
   authKey.value = undefined
+  checkingUsername.value = false
+  username.value = undefined
+  newUsername.value = undefined
+  settingUsername.value = false
+  playingAsGuest.value = false
 }
 
 const createLnurlAuth = async () => {
@@ -93,7 +141,12 @@ const createLnurlAuth = async () => {
 
 onBeforeMount(() => {
   document.addEventListener('visibilitychange', async (event) => {
-    if (document.visibilityState !== 'visible' || lnurlHash.value == null) {
+    if (
+      document.visibilityState !== 'visible'
+      || lnurlHash.value == null
+      || authKey.value != null
+      || authenticating.value
+    ) {
       return
     }
     authenticating.value = true
@@ -102,6 +155,7 @@ onBeforeMount(() => {
       if (response.data.status === 'success') {
         authKey.value = response.data.data
         authenticating.value = false
+        checkUsername()
         return
       }
     } catch (error) {}
@@ -109,10 +163,47 @@ onBeforeMount(() => {
   })
 })
 
+const checkUsername = async () => {
+  checkingUsername.value = true
+  try {
+    const response = await axios.get(`https://n8n.sate.tools/webhook/45c135b7-b708-44ca-86cd-e057834b0a20/blocktris/user/${authKey.value}`)
+    username.value = response.data.Name
+  } catch (error) {}
+  checkingUsername.value = false
+}
+
+const setUsername = async () => {
+  if (
+    authKey.value == null
+    || newUsername.value == null
+    || newUsername.value.length === 0
+  ) {
+    return
+  }
+  settingUsername.value = true
+  const url = `https://n8n.sate.tools/webhook/blocktris/user`
+  const formdata = new FormData()
+  formdata.append('key', authKey.value)
+  formdata.append('name', newUsername.value)
+  const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow',
+  }
+  await fetch(url, requestOptions as any)
+  username.value = newUsername.value
+  settingUsername.value = false
+}
+
+const playAsGuest = () => {
+  playingAsGuest.value = true
+}
+
 /////
 // SOCKET GAMES SERVER
 const connecting = ref(true)
 const connected = ref(false)
+const playing = ref(false)
 
 let socketGames: SocketGames
 onBeforeMount(() => {
@@ -130,6 +221,10 @@ onBeforeMount(() => {
   })
 })
 
+
+const play = () => {
+  socketGames.emit('play')
+}
 const turn = () => {
   socketGames.emit('turn')
 }
