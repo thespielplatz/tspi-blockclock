926887f5ae5bdc3d75c19c0ec4652d55cf56e68e
thespielplatz <informatics@gmx.net>
2022-11-10 13:05:06 +0100

    [REFACTOR] reverted lines to .env

--- text.js
+++ text.js
@@ -8,9 +8,10 @@ const FPS = process.env.DISPLAY_FPS || 60
 const WIDTH = process.env.DISPLAY_WIDTH || 50
 const HEIGHT = process.env.DISPLAY_HEIGHT || 5
 const BRIGHTNESS = process.env.DISPLAY_BRIGHTNESS || 50
+const REVERTED_ROWS = process.env.DISPLAY_REVERTED_ROWS || '1,3'
 const NUM_LEDS = WIDTH * HEIGHT
 
-const renderer = new WS281xRenderer(NUM_LEDS, BRIGHTNESS, WIDTH)
+const renderer = new WS281xRenderer(NUM_LEDS, BRIGHTNESS, WIDTH, REVERTED_ROWS)
 renderer.init()
 
 display.init(WIDTH, HEIGHT)
